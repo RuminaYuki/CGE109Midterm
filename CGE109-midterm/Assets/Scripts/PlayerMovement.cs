b51c59b1e949f4d63cabc61a818eb603ce74026e
SDFcl <142718493+SDFcl@users.noreply.github.com>
2025-09-15 12:19:19 +0700

    15/9/68
    fix-update : player slope

--- CGE109-midterm/Assets/Scripts/PlayerMovement.cs
+++ CGE109-midterm/Assets/Scripts/PlayerMovement.cs
@@ -19,6 +19,9 @@ public class PlayerMovement : MonoBehaviour
 
     public CapsuleCollider Collider;
 
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+
     private float speed = 1;
     private void Awake()
     {
@@ -33,22 +36,20 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, (playheight * 0.5f) + 0.9f, Environment);
+        grounded = Physics.Raycast(transform.position, Vector3.down, (playheight * 0.5f) + 1.25f, Environment);
         if (grounded)
         {
             rb.linearDamping = groundDrag;
         }
         else if (!grounded) { rb.linearDamping = 0; }
-        Debug.Log(rb.linearDamping);
-        Debug.Log(grounded);
 
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-
+        Moveplayer();
         MoveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rb.AddForce(MoveDirection.normalized * moveSpeed * 10f * speed, ForceMode.Force);
+        
         Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
 
         if (flatVel.magnitude > moveSpeed)
@@ -71,5 +72,35 @@ public class PlayerMovement : MonoBehaviour
         }
 
 
+    }
+
+    private void Moveplayer()
+    {
+        if (OnSlope())
+        {
+            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 5f * speed, ForceMode.Force);
+        }
+        else
+        {
+            rb.AddForce(MoveDirection.normalized * moveSpeed * 10f * speed, ForceMode.Force);
+        }
+
+        rb.useGravity = !OnSlope();
+    }
+
+    private bool OnSlope()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playheight*0.5f + 1.25f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+
+        return false;
+    }
+
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(MoveDirection, slopeHit.normal).normalized;
     }
 }
