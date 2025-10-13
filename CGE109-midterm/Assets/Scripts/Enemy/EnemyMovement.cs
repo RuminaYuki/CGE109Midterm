cb3d828dcb1e29e9232b2ed22cd8f6795b07043b
SDFcl <142718493+SDFcl@users.noreply.github.com>
2025-10-13 14:33:57 +0700

    13/10/68

--- CGE109-midterm/Assets/Scripts/Enemy/EnemyMovement.cs
+++ CGE109-midterm/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,7 +1,22 @@
-using UnityEngine;public class EnemyMovement : MonoBehaviour{    private CharacterController controller;    private Vector3 velocity;    public Transform groundCheck;    public float gravity = -9.81f;    public float groundDistance = 0.4f;    public LayerMask environmentMask;    // Start is called once before the first execution of Update after the MonoBehaviour is created    void Start()    {        controller = GetComponent<CharacterController>();    }    // Update is called once per frame    void Update()    {        velocity.y += gravity * Time.deltaTime;        controller.Move(velocity* Time.deltaTime);    }    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag(""))
-        {
+using TMPro;
+using UnityEngine;
+using UnityEngine.AI;
 
-        }
-    }}
+public class EnemyMovement : MonoBehaviour
+{
+    private NavMeshAgent Agent;
+
+    public Vector3 m_Position;
+
+    private void Awake()
+    {       
+        Agent = GetComponent<NavMeshAgent>();
+        m_Position = Agent.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Agent.SetDestination(m_Position);
+    }
+}
--- CGE109-midterm/Assets/Scripts/Environment/BronkenGlassSound.cs
+++ CGE109-midterm/Assets/Scripts/Environment/BronkenGlassSound.cs
@@ -3,37 +3,26 @@
 public class BronkenGlassSound : MonoBehaviour
 {
     //[SerializeField] private MeshCollider objCollider;
-    //[SerializeField] private Collider noiseCollider;
+    [SerializeField] private Collider noiseCollider;
     [SerializeField] private float activeTime = 0.3f;
 
     private void Awake()
     {
-        //noiseCollider.enabled = false;
+        noiseCollider.enabled = false;
     }
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //noiseCollider.enabled = true;
             Debug.Log("🦶 Glass broken - Noise emitted!");
+            noiseCollider.enabled = true;
             Invoke(nameof(DisableNoise), activeTime);
         }
     }
 
     private void DisableNoise()
     {
-        //noiseCollider.enabled = false;
+        noiseCollider.enabled = false;
         Debug.Log("🦶 Glass broken - Off");
     }
 }
