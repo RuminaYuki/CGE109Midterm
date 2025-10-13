f4bd08b83e1fd0ccd833d5c9b607bd6b0e19edec
SDFcl <142718493+SDFcl@users.noreply.github.com>
2025-10-13 09:56:53 +0700

    13/10/68

---
+++ CGE109-midterm/Assets/Scripts/Environment/BronkenGlassSound.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class BronkenGlassSound : MonoBehaviour
+{
+    //[SerializeField] private MeshCollider objCollider;
+    //[SerializeField] private Collider noiseCollider;
+    [SerializeField] private float activeTime = 0.3f;
+
+    private void Awake()
+    {
+        //noiseCollider.enabled = false;
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //noiseCollider.enabled = true;
+            Debug.Log("🦶 Glass broken - Noise emitted!");
+            Invoke(nameof(DisableNoise), activeTime);
+        }
+    }
+
+    private void DisableNoise()
+    {
+        //noiseCollider.enabled = false;
+        Debug.Log("🦶 Glass broken - Off");
+    }
+}
